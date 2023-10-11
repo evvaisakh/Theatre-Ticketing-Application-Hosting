589238def8a8875f2435ea12fe830bf0daa02320
Vaisakh Vijayan <evvaisakh4@gmail.com>
2023-10-11 16:24:34 +0530

    updated

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ app.use(morgan('dev'));
 const cors = require('cors');
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
 const api = require('./routes/router');
-app.use('/ticketMaster', api);
+app.use('/', api);
 
 const db = require('./db/connection');
 require('dotenv').config();
 
-const path =require('path');
+app.use(express.static(path.join(__dirname, 'frontend')));
 
-app.use(express.static(path.join(__dirname,'frontend')))
-
-app.get('*',async(req,res)=>{
-    res.sendFile(path.join(__dirname,'frontend','index.html'))
+app.get('*', async(req, res)=>{
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 app.listen(PORT, () => {
